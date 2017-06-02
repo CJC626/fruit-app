f7be3b7819fe4a81596d90905e20d687f0405d2d
CJC626 <CJC626@gmail.com>
2017-06-02 09:07:34 -0400

    Moved initialization of fruit tree to service.

--- app/fruit-shelf/fruit-shelf.component.ts
+++ app/fruit-shelf/fruit-shelf.component.ts
@@ -2,30 +2,27 @@ import { Component } from '@angular/core';
 import { Fruit } from '../fruit';
 import { Observable } from 'rxjs/observable';
 
+import { FruitService } from '../fruit-service/fruit.service';
+
 @Component({
   selector: 'fruit-shelf',
   templateUrl: 'app/fruit-shelf/fruit-shelf.component.html',
   styleUrls: ['app/fruit-shelf/fruit-shelf.component.css']
 })
 export class FruitShelfComponent  { 
-  fruits : Fruit[] = [
-        {"id":1, "name":"apple", "size":2, "color":"maroon", "stockAmt": 5},
-        {"id":2,"name":"orange", "size":2, "color":"orange", "stockAmt": 5},
-        {"id":3,"name":"avocado", "size":2, "color":"darkgreen", "stockAmt": 5},
-        {"id":4,"name":"peach", "size":2, "color":"lightcoral", "stockAmt": 5},
-        {"id":5,"name":"banana", "size":2, "color":"yellow", "stockAmt": 5},
-        {"id":6,"name":"tomato", "size":2, "color":"red", "stockAmt": 5},
-        {"id":7,"name":"blueberry", "size":2, "color":"darkblue", "stockAmt": 5},
-        {"id":8,"name":"strawberry", "size":2, "color":"crimson", "stockAmt": 5},
-        {"id":9,"name":"whitegrape", "size":2, "color":"lightgreen", "stockAmt": 5},
-        {"id":10,"name":"redgrape", "size":2, "color":"purple", "stockAmt": 5},
-        {"id":11,"name":"mango", "size":2, "color":"lightcoral", "stockAmt": 5},
-        {"id":12,"name":"lemon", "size":2, "color":"yellow", "stockAmt": 5},
-        {"id":13,"name":"lime", "size":2, "color":"green", "stockAmt": 5}
-  ];
-  fruitTree = this.splitFruits(this.fruits);
+
+  constructor(private fruitService:FruitService){
+    
+  }
+
+  fruitTree : Array<Array<Fruit>>;
   name = 'Angular'; 
 
+  ngOnInit():void{
+    this.fruitService.getFruits().then(fruits => {
+      this.fruitTree = this.splitFruits(fruits);
+    });
+  }
 
   splitFruits(frs:Fruit[]):Array<Array<Fruit>>{
       let fruitDisp = new Array<Array<Fruit>>();
